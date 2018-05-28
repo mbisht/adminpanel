4c8786ea18fc50c5562f072980b755f9e29ffc19
mbisht <mayankbisht.com@gmail.com>
2018-05-29 02:15:15 +0530

    First commit

---
+++ config.php
@@ -0,0 +1,4 @@
+<?php
+define('DIR_APPLICATION', '/home/webliststore/public_html/demo/techno_dental_lab.com/admin/'); 
+define('DIR_SYSTEM', 'https://webliststore.club/demo/techno_dental_lab.com/admin/');
+?>
